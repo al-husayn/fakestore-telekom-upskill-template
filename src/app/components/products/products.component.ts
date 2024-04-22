a755d9ea5bbca52c3dc9f09fac15f861eab0a52c
Al-Hussein Abubakar <82461094+al-husayn@users.noreply.github.com>
2024-04-22 04:05:44 +0000

    implement product list

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductlistService } from 'src/app/services/productlist.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  public productsList: any;
 
+  public constructor(private products: ProductlistService) {}
+
+  public ngOnInit(): void {
+    this.products.getProduct().subscribe((response) => {
+      this.productsList = response;
+      console.log(this.productsList);
+    });
+  }
 }
